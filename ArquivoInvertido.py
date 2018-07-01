0540c3470a436043241ab8e4dfd4face630d85dd
luisdelgado <lhds@cin.ufpe.br>
2018-07-01 00:05:45 -0300

    Começando a ler os arquivos em remover_html

--- ArquivoInvertido.py
+++ ArquivoInvertido.py
@@ -1,11 +1,16 @@
 import datetime
-import time
 
 
 class ArquivoInvertido:
 
+    def remover_html(selected_file, number_visited):
+        with open('arquivos/' + str(selected_file) + '/otimizado/' + str(number_visited) + ".html") as file:
+            data = file.read()
+            print(data)
+
     if __name__ == '__main__':
 
         inicio = (datetime.datetime.now().time())
+        remover_html(1, 1)
         fim = (datetime.datetime.now().time())
         print(inicio, fim)

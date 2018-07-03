b4b6d5ac8c3e1da3cdf7aac52624d3d9223f5f7d
luisdelgado <lhds@cin.ufpe.br>
2018-07-03 00:46:59 -0300

    Removendo html tags

--- ArquivoInvertido.py
+++ ArquivoInvertido.py
@@ -4,9 +4,24 @@ import datetime
 class ArquivoInvertido:
 
     def remover_html(selected_file, number_visited):
+
+        # Abrindo arquivos coletados pelo crawler
         with open('arquivos/' + str(selected_file) + '/otimizado/' + str(number_visited) + ".html") as file:
-            data = file.read()
-            print(data)
+            for line in file:
+
+                # Verificando a existência de tags para removê-las dos arquivos finais
+                while line.find("<") > -1:
+                    if line.find("<") > -1:
+                        if line.find(">") > -1:
+                            new_substring = line[line.find("<"):line.find(">")+1]
+                            new_line = line.replace(new_substring, "")
+                            line = new_line
+                        else:
+                            line = ""
+                    else:
+                        if line.find(">") > -1:
+                            line = ""
+                print(line)
 
     if __name__ == '__main__':
 

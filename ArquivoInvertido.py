3bf95fe9fd3d6968d1dcfc01f9b5d364d565ce90
luisdelgado <lhds@cin.ufpe.br>
2018-07-04 01:35:13 -0300

    Pegando todas as palavras de um site

--- ArquivoInvertido.py
+++ ArquivoInvertido.py
@@ -42,14 +42,35 @@ class ArquivoInvertido:
             print(number_visited / 10, "%")
             number_visited = number_visited + 1
 
+    def criando_arquivo(selected_file):
+        number_visited = 1
+        while number_visited < 1000:
+            try:
+
+                # Abrindo arquivos coletados pelo crawler
+                with open('invertido/{0}/{1}.html'.format(str(selected_file), str(number_visited))) as file:
+                    end_word = ""
+                    for line in file:
+                        for word in line:
+                            if word != " " and word != '\n':
+                                end_word = end_word + word
+                            else:
+                                if end_word != "":
+                                    print(end_word)
+                                    end_word = ""
+
+            except:
+                pass
+
     if __name__ == '__main__':
 
         inicio = (datetime.datetime.now().time())
         sites = 1
         print("inicio remover html")
-        while (sites < 7):
-            print(sites, datetime.datetime.now().time())
-            remover_html(sites)
-            sites = sites + 1
+        #while (sites < 7):
+        #    print(sites, datetime.datetime.now().time())
+        #    remover_html(sites)
+        #    sites = sites + 1
+        criando_arquivo(1)
         fim = (datetime.datetime.now().time())
         print("inicio", inicio, "fim", fim)

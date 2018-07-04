70088189439e4f27f623cc7cd9171037249dd8fa
luisdelgado <lhds@cin.ufpe.br>
2018-07-04 11:46:01 -0300

    Salvando todas as palavras de um site informando o arquivo

--- ArquivoInvertido.py
+++ ArquivoInvertido.py
@@ -1,6 +1,34 @@
 import datetime
 import traceback
+from collections import OrderedDict
 
+def verificar_word(selected_file, number_visited, end_word):
+    searched = False
+    end_file = ""
+    number_document = str(selected_file) + str(number_visited)
+    with open('invertido/arquivo.txt') as file:
+        for line in file:
+            if line.find(end_word) > -1:
+                if line.find(str(number_document)) == -1:
+                    for word in line:
+                        if word == ' \n':
+                            line = word.replace(" \n", ("- " + str(selected_file) + str(number_visited)) + " \n")
+                            searched = True
+
+            end_file = end_file + line
+
+        if searched:
+            pass
+        else:
+            end_file = end_file + end_word + ': ' + str(selected_file) + str(number_visited) + " \n"
+
+        end_file = "\n".join(list(OrderedDict.fromkeys(end_file.split("\n"))))
+
+    #print(end_file)
+    # Escrevendo no arquivo
+    f = open('invertido/arquivo.txt', 'w+')
+    f.write(str(end_file))
+    f.close()
 
 class ArquivoInvertido:
 
@@ -47,7 +75,7 @@ class ArquivoInvertido:
         while number_visited < 1000:
             try:
 
-                # Abrindo arquivos coletados pelo crawler
+                # Abrindo arquivos sem HTML
                 with open('invertido/{0}/{1}.html'.format(str(selected_file), str(number_visited))) as file:
                     end_word = ""
                     for line in file:
@@ -56,11 +84,15 @@ class ArquivoInvertido:
                                 end_word = end_word + word
                             else:
                                 if end_word != "":
-                                    print(end_word)
+                                    end_file = verificar_word(selected_file, number_visited, end_word)
                                     end_word = ""
 
             except:
                 pass
+
+            number_visited = number_visited + 1
+            print(number_visited / 10, "%")
+
 
     if __name__ == '__main__':
 
